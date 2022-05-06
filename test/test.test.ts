d6552b9fc22fe650f4f095ff9dc431662c74fdfb
Steven Xu <steven.xu@laserfiche.com>
2022-05-06 19:54:26 -0400

    added a small test case

---
+++ test/test.test.ts
@@ -0,0 +1,8 @@
+import {client,getAccessToken,repoId,entryId} from "./config";
+console.log("test");
+describe("Create access token", () => {
+
+    test("Get the Access Token", async () => {
+        console.log(repoId);
+    });
+})
