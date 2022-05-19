b04202abb1ba1f70cedf489f134bb8cab60d2555
Steven Xu <steven.xu@laserfiche.com>
2022-05-19 13:03:04 -0400

    added the for each implementation

--- src/SearchClient.ts
+++ src/SearchClient.ts
@@ -2,6 +2,24 @@ import {ClientOptions} from './ClientHelper';
 import {ODataValueContextOfIListOfContextHit, ODataValueContextOfIListOfODataGetSearchResults} from './index';
 
 export class SearchClient extends ClientOptions{
+    async GetSearchResultsForEach(callback: (response: ODataValueContextOfIListOfODataGetSearchResults) => Promise<boolean>, repoId:string, searchToken:string, groupByEntryType?:boolean, refresh?:boolean, fields?:string[], formatFields?:boolean, prefer?:string, culture?:string, select?:string, orderby?:string, top?:number,skip?:number, count?:boolean, maxPageSize?:number): Promise<void> {
+        var response = await this.getSearchResults(repoId, searchToken, groupByEntryType, refresh, fields, formatFields, this.CreateMaxPageSizePreferHeaderPayload(maxPageSize), culture, select, orderby, top, skip, count);
+        let nextLink = response.odataNextLink;
+        while (await callback(response) && nextLink != null)
+        {
+            response = await this.getNextLinkListing<ODataValueContextOfIListOfODataGetSearchResults>(this.http, this.processGetSearchResults, nextLink, maxPageSize);
+            nextLink = response.odataNextLink;
+        }
+    }
+    async GetSearchContextHitsForEach(callback: (response: ODataValueContextOfIListOfContextHit) => Promise<boolean>, repoId:string, searchToken:string, rowNumber:number,prefer?:string,select?:string, orderby?:string,top?:number,skip?:number,count?:boolean,maxPageSize?:number): Promise<void> {
+        var response = await this.getSearchContextHits(repoId, searchToken, rowNumber, this.CreateMaxPageSizePreferHeaderPayload(maxPageSize), select, orderby, top, skip, count);
+        let nextLink = response.odataNextLink;
+        while (await callback(response) && nextLink != null)
+        {
+            response = await this.getNextLinkListing<ODataValueContextOfIListOfContextHit>(this.http, this.processGetSearchContextHits, nextLink, maxPageSize);
+            nextLink = response.odataNextLink;
+        }
+    }
     async GetSearchResultsNextLink(nextLink: string, maxPageSize?: number): Promise<ODataValueContextOfIListOfODataGetSearchResults> {
         return await this.getNextLinkListing<ODataValueContextOfIListOfODataGetSearchResults>(this.http, this.processGetSearchResults, nextLink, maxPageSize);
     }
--- src/TemplateDefinitionsClient.ts
+++ src/TemplateDefinitionsClient.ts
@@ -2,6 +2,36 @@ import {ClientOptions} from './ClientHelper';
 import {ODataValueContextOfIListOfWTemplateInfo, ODataValueContextOfIListOfTemplateFieldInfo} from './index';
 
 export class TemplateDefinitionsClient extends ClientOptions{
+    async GetTemplateDefinitionsForEach(callback: (response: ODataValueContextOfIListOfWTemplateInfo) => Promise<boolean>, repoId:string, templateName?:string, prefer?:string, culture?:string, select?:string, orderby?:string,top?:number,skip?:number,count?:boolean, maxPageSize?:number): Promise<void> {
+        var response = await this.getTemplateDefinitions(repoId, templateName, this.CreateMaxPageSizePreferHeaderPayload(maxPageSize), culture, select, orderby, top, skip, count);
+        let nextLink = response.odataNextLink;
+        while (await callback(response) && nextLink != null)
+        {
+            response = await this.getNextLinkListing<ODataValueContextOfIListOfWTemplateInfo>(this.http, this.processGetTemplateDefinitions, nextLink, maxPageSize);
+            nextLink = response.odataNextLink;
+        }
+    }
+
+    async GetTemplateFieldDefinitionsForEach(callback: (response: ODataValueContextOfIListOfTemplateFieldInfo) => Promise<boolean>, repoId:string, templateId:number,prefer?:string,culture?:string, select?:string, orderby?:string, top?:number, skip?:number, count?:boolean,maxPageSize?:number): Promise<void> {
+        var response = await this.getTemplateFieldDefinitions(repoId, templateId, this.CreateMaxPageSizePreferHeaderPayload(maxPageSize), culture, select, orderby, top, skip, count);
+        let nextLink = response.odataNextLink;
+        while (await callback(response) && nextLink != null)
+        {
+            response = await this.getNextLinkListing<ODataValueContextOfIListOfTemplateFieldInfo>(this.http, this.processGetTemplateFieldDefinitions, nextLink, maxPageSize);
+            nextLink = response.odataNextLink;
+        }
+    }
+
+    async GetTemplateFieldDefinitionsByTemplateNameForEach(callback: (response: ODataValueContextOfIListOfTemplateFieldInfo) => Promise<boolean>, repoId:string, templateName:string, prefer?:string, culture?:string, select?:string, orderby?:string,top?:number,skip?:number, count?:boolean,maxPageSize?:number): Promise<void> {
+        var response = await this.getTemplateFieldDefinitionsByTemplateName(repoId, templateName, this.CreateMaxPageSizePreferHeaderPayload(maxPageSize), culture, select, orderby, top, skip, count);
+        let nextLink = response.odataNextLink;
+        while (await callback(response) && nextLink != null)
+        {
+            response = await this.getNextLinkListing<ODataValueContextOfIListOfTemplateFieldInfo>(this.http, this.processGetTemplateFieldDefinitionsByTemplateName, nextLink, maxPageSize);
+            nextLink = response.odataNextLink;
+        }
+    }
+
     async getTemplateDefinitionsNextLink(nextLink: string, maxPageSize?: number): Promise<ODataValueContextOfIListOfWTemplateInfo> {
         return await this.getNextLinkListing<ODataValueContextOfIListOfWTemplateInfo>(this.http, this.processGetTemplateDefinitions, nextLink, maxPageSize);
     }
