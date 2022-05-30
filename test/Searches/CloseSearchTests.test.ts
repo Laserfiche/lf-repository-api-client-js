c7105a068cbaad5b9b17803c31906d0b91b6565a
Steven Xu <steven.xu@laserfiche.com>
2022-05-30 14:46:42 -0400

    included but skipped search tests

--- test/Searches/CloseSearchTests.test.ts
+++ test/Searches/CloseSearchTests.test.ts
@@ -1,26 +1,22 @@
-import {client, getAccessTokenForTests, repoId} from "../config";
-import {AdvancedSearchRequest} from "../../src";
+import { testKey, testServicePrincipalKey, repoId, baseUrlDebug } from '../testHelper.js';
+import { RepositoryApiClient, IRepositoryApiClient } from '../../src/ClientBase.js';
+import { AdvancedSearchRequest } from '../../src/index.js';
 
-describe("Search Tests", () => {
-    let token = "";
-    beforeEach(async() =>{
-        token = await getAccessTokenForTests();
-    });
-    afterEach(async() =>{
-        token = "";
-    });
+describe.skip('Search Integration Tests', () => {
+  let _RepositoryApiClient: IRepositoryApiClient;
+  beforeEach(() => {
+    _RepositoryApiClient = RepositoryApiClient.create(testServicePrincipalKey, JSON.stringify(testKey), baseUrlDebug);
+  });
+  test('Close Search Operations', async () => {
+    //create search
+    let request = new AdvancedSearchRequest();
+    request.searchCommand = '({LF:Basic ~= "search text", option="DFANLT"})';
+    var response = await _RepositoryApiClient.searchesClient.createSearchOperation({ repoId, request });
+    let searchToken = response.toJSON().token;
+    expect(searchToken).not.toBeNull();
 
-    test("Close Search Operations", async () => {
-
-        //create search
-        let request = new AdvancedSearchRequest();
-        request.searchCommand = "({LF:Basic ~= \"search text\", option=\"DFANLT\"})";
-        var response = await client.createSearchOperation(repoId,request);
-        let searchToken = response.toJSON().token;
-        expect(searchToken).not.toBeNull();
-
-        //close the search
-        var closeSearchResponse = await client.cancelOrCloseSearch(repoId, searchToken);
-        expect(closeSearchResponse.toJSON().value).toBe(true);
-    });
-})
+    //close the search
+    var closeSearchResponse = await _RepositoryApiClient.searchesClient.cancelOrCloseSearch({ repoId, searchToken });
+    expect(closeSearchResponse.toJSON().value).toBe(true);
+  });
+});
--- test/Searches/SearchTests.test.ts
+++ test/Searches/SearchTests.test.ts
@@ -1,81 +1,84 @@
-import {client, getAccessTokenForTests, repoId} from "../config";
-import {jest} from '@jest/globals';
-import {AdvancedSearchRequest} from "../../src";
+import { testKey, testServicePrincipalKey, repoId, baseUrlDebug } from '../testHelper.js';
+import { RepositoryApiClient, IRepositoryApiClient } from '../../src/ClientBase.js';
+import { AdvancedSearchRequest } from '../../src/index.js';
+import {jest} from '@jest/globals'
 
-let searchToken = "test";
-describe("Search Tests", () => {
-    let token = "";
-    beforeEach(async() =>{
-        token = await getAccessTokenForTests();
-        searchToken = "test2";
-    });
-    jest.setTimeout(20000);
-    afterEach(async() =>{
-        if (searchToken != "" || searchToken != null){ 
-            await client.cancelOrCloseSearch(repoId,searchToken);
-            await new Promise((r) => setTimeout(r, 5000));
-        }
-        token = "";
-    });
-    //not done yet
-    test.skip("Create Search Operation", async () => {
+let searchToken = 'test';
+describe.skip('Search Integration Tests Part 2', () => {
+  let _RepositoryApiClient: IRepositoryApiClient;
+  beforeEach(() => {
+    _RepositoryApiClient = RepositoryApiClient.create(testServicePrincipalKey, JSON.stringify(testKey), baseUrlDebug);
+  });
+  jest.setTimeout(20000);
+  afterEach(async () => {
+    if (searchToken != '' || searchToken != null) {
+      await _RepositoryApiClient.searchesClient.cancelOrCloseSearch({ repoId, searchToken });
+      await new Promise((r) => setTimeout(r, 5000));
+    }
+  });
+  // //not done yet
+  // test.skip("Create Search Operation", async () => {
 
-        //create search
-        let request = new AdvancedSearchRequest();
-        request.searchCommand = "({LF:Basic ~= \"search text\", option=\"DFANLT\"})";
-        var response = await client.createSearchOperation(repoId,request);
-        searchToken = response.toJSON().token;
-        expect(searchToken).not.toBeNull();
-        expect(searchToken).not.toBe("");
-        let webURL = response.toJSON().Headers;
-        console.log(response);
-        console.log(webURL);
-    });
+  //     //create search
+  //     let request = new AdvancedSearchRequest();
+  //     request.searchCommand = "({LF:Basic ~= \"search text\", option=\"DFANLT\"})";
+  //     var response = await client.createSearchOperation(repoId,request);
+  //     searchToken = response.toJSON().token;
+  //     expect(searchToken).not.toBeNull();
+  //     expect(searchToken).not.toBe("");
+  //     let webURL = response.toJSON().Headers;
+  //     console.log(response);
+  //     console.log(webURL);
+  // });
 
-    jest.setTimeout(20000);
-    test("Get Search Context Hits", async ()=>{
-        let request = new AdvancedSearchRequest();
-        request.searchCommand = "({LF:Basic ~= \"*\", option=\"DFANLT\"})";
-        var searchResponse = await client.createSearchOperation(repoId,request);
-        searchToken = searchResponse.toJSON().token;
-        expect(searchToken).not.toBeNull();
-        expect(searchToken).not.toBe("");
-        await new Promise((r) => setTimeout(r, 5000));
-        var searchResultsResponse = await client.getSearchResults(repoId,searchToken);
-        var searchResults = searchResultsResponse.toJSON().value;
-        expect(searchResults).not.toBeNull();
-        expect(searchResults.length > 0).toBeTruthy();
-        let rowNum = searchResults[0].rowNumber;
+  jest.setTimeout(20000);
+  test('Get Search Context Hits', async () => {
+    let request = new AdvancedSearchRequest();
+    request.searchCommand = '({LF:Basic ~= "*", option="DFANLT"})';
+    var searchResponse = await _RepositoryApiClient.searchesClient.createSearchOperation({ repoId, request });
+    searchToken = searchResponse.toJSON().token;
+    expect(searchToken).not.toBeNull();
+    expect(searchToken).not.toBe('');
+    await new Promise((r) => setTimeout(r, 5000));
+    var searchResultsResponse = await _RepositoryApiClient.searchesClient.getSearchResults({ repoId, searchToken });
+    var searchResults = searchResultsResponse.toJSON().value;
+    expect(searchResults).not.toBeNull();
+    expect(searchResults.length > 0).toBeTruthy();
+    let rowNum = searchResults[0].rowNumber;
 
-        var contextHitResponse = await client.getSearchContextHits(repoId,searchToken,rowNum);
-        var contextHits = contextHitResponse.toJSON().value;
-        expect(contextHits).not.toBeNull();
+    var contextHitResponse = await _RepositoryApiClient.searchesClient.getSearchContextHits({
+      repoId,
+      searchToken,
+      rowNumber: rowNum,
     });
-    jest.setTimeout(20000);
-    test("Get Search Results", async ()=>{
-        let request = new AdvancedSearchRequest();
-        request.searchCommand = "({LF:Basic ~= \"search text\", option=\"DFANLT\"})";
-        var searchResponse = await client.createSearchOperation(repoId,request);
-        searchToken = searchResponse.toJSON().token;
-        expect(searchToken).not.toBeNull();
-        expect(searchToken).not.toBe("");
-        await new Promise((r) => setTimeout(r, 10000));
-        var searchResultsResponse = await client.getSearchResults(repoId,searchToken);
-        var searchResults = searchResultsResponse.toJSON().value;
-        expect(searchResults).not.toBeNull();
-    });
-    jest.setTimeout(20000);
-    test("Get Search Status", async ()=>{
-        let request = new AdvancedSearchRequest();
-        request.searchCommand = "({LF:Basic ~= \"search text\", option=\"DFANLT\"})";
-        var searchResponse = await client.createSearchOperation(repoId,request);
-        searchToken = searchResponse.toJSON().token;
-        expect(searchToken).not.toBeNull();
-        expect(searchToken).not.toBe("");
-        await new Promise((r) => setTimeout(r, 5000));
-        var searchStatusResponse = await client.getSearchStatus(repoId,searchToken);
-        var searchStatus = searchStatusResponse.toJSON();
-        expect(searchStatus).not.toBeNull();
-        expect(searchStatus.operationToken).toBe(searchToken);
-    });
-})
+    var contextHits = contextHitResponse.toJSON().value;
+    expect(contextHits).not.toBeNull();
+  });
+  jest.setTimeout(20000);
+  test('Get Search Results', async () => {
+    let request = new AdvancedSearchRequest();
+    request.searchCommand = '({LF:Basic ~= "search text", option="DFANLT"})';
+    var searchResponse = await _RepositoryApiClient.searchesClient.createSearchOperation({ repoId, request });
+    searchToken = searchResponse.toJSON().token;
+    expect(searchToken).not.toBeNull();
+    expect(searchToken).not.toBe('');
+    await new Promise((r) => setTimeout(r, 10000));
+    var searchResultsResponse = await _RepositoryApiClient.searchesClient.getSearchResults({ repoId, searchToken });
+    var searchResults = searchResultsResponse.toJSON().value;
+    expect(searchResults).not.toBeNull();
+  });
+  jest.setTimeout(20000);
+  test('Get Search Status', async () => {
+    let request = new AdvancedSearchRequest();
+    request.searchCommand = '({LF:Basic ~= "search text", option="DFANLT"})';
+    var searchResponse = await _RepositoryApiClient.searchesClient.createSearchOperation({ repoId, request });
+    searchToken = searchResponse.toJSON().token;
+    expect(searchToken).not.toBeNull();
+    expect(searchToken).not.toBe('');
+    await new Promise((r) => setTimeout(r, 5000));
+    var searchStatusResponse = await _RepositoryApiClient.searchesClient.getSearchStatus({ repoId, searchToken });
+    var searchStatus = searchStatusResponse.toJSON();
+    expect(searchStatus).not.toBeNull();
+    expect(searchStatus.operationToken).toBe(searchToken);
+  });
+});
