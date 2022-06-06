b2f4ebcc6b2556055dba5cf0cdb142d963d31edc
Steven Xu <steven.xu@laserfiche.com>
2022-06-06 18:21:29 -0400

    reformatt files

--- test/BaseTest.ts
+++ test/BaseTest.ts
@@ -1,23 +1,28 @@
-import {Entry,PostEntryChildrenRequest, EntryType, WFieldInfo, PostEntryChildrenEntryType} from "../src";
-import {repoId } from './testHelper.js';
-import {IRepositoryApiClient} from '../src/ClientBase.js';
+import { Entry, PostEntryChildrenRequest, EntryType, WFieldInfo, PostEntryChildrenEntryType } from '../src';
+import { repoId } from './testHelper.js';
+import { IRepositoryApiClient } from '../src/ClientBase.js';
 
-export async function CreateEntry(client: IRepositoryApiClient, entryName: string | undefined, parentEntryId:number = 1, autoRename:boolean = true):Promise<Entry>{
-    var request = new PostEntryChildrenRequest();
-    request.entryType = PostEntryChildrenEntryType.Folder;
-    request.name = entryName;
-    var newEntry = await client.entriesClient.createOrCopyEntry({repoId,entryId:parentEntryId,request, autoRename});
-    expect(newEntry).not.toBeNull();
-    expect(newEntry.parentId).toBe(parentEntryId);
-    expect(newEntry.entryType).toBe(EntryType.Folder);
-    return newEntry;
+export async function CreateEntry(
+  client: IRepositoryApiClient,
+  entryName: string | undefined,
+  parentEntryId: number = 1,
+  autoRename: boolean = true
+): Promise<Entry> {
+  var request = new PostEntryChildrenRequest();
+  request.entryType = PostEntryChildrenEntryType.Folder;
+  request.name = entryName;
+  var newEntry = await client.entriesClient.createOrCopyEntry({ repoId, entryId: parentEntryId, request, autoRename });
+  expect(newEntry).not.toBeNull();
+  expect(newEntry.parentId).toBe(parentEntryId);
+  expect(newEntry.entryType).toBe(EntryType.Folder);
+  return newEntry;
 }
 
-export async function allFalse(arr:WFieldInfo[]):Promise<boolean>{
-    for(let i = 0; i < arr.length;i++){
-        if (arr[i].isRequired == true){
-            return false;
-        }
+export async function allFalse(arr: WFieldInfo[]): Promise<boolean> {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].isRequired == true) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
--- test/testHelper.ts
+++ test/testHelper.ts
@@ -1,15 +1,14 @@
 import 'dotenv/config';
-import { AccessKey} from '@laserfiche/lf-api-client-core';
-import {StringUtils} from '@laserfiche/lf-js-utils';
+import { AccessKey } from '@laserfiche/lf-api-client-core';
+import { StringUtils } from '@laserfiche/lf-js-utils';
 export const testServicePrincipalKey: string =
   process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY ?? '';
 if (!testServicePrincipalKey) {
   throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
 }
 let accessKeyBase64: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY ?? '';
-if (!accessKeyBase64){
+if (!accessKeyBase64) {
   throw new Error(`Unable to load accessKey from .env`);
 }
 export const OAuthAccessKey: AccessKey = JSON.parse(StringUtils.base64toString(accessKeyBase64) ?? '');
 export const repoId: string = process.env.DEV_CA_PUBLIC_USE_REPOSITORY_ID_1 ?? '';
-
