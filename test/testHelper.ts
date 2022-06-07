e1b6085b584faaa61d660b9188e10c94f0438a57
Steven Xu <steven.xu@laserfiche.com>
2022-06-07 18:37:51 -0400

    changed environment variables and unskipped test

--- test/testHelper.ts
+++ test/testHelper.ts
@@ -2,14 +2,14 @@ import 'dotenv/config';
 import { AccessKey} from '@laserfiche/lf-api-client-core';
 import {StringUtils} from '@laserfiche/lf-js-utils';
 export const testServicePrincipalKey: string =
-  process.env.DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY ?? '';
+  process.env.SERVICE_PRINCIPAL_KEY ?? '';
 if (!testServicePrincipalKey) {
-  throw new Error(`Unable to load DEV_CA_PUBLIC_USE_TESTOAUTHSERVICEPRINCIPAL_SERVICE_PRINCIPAL_KEY from .env`);
+  throw new Error(`Unable to load SERVICE_PRINCIPAL_KEY from .env`);
 }
-let accessKeyBase64: string = process.env.DEV_CA_PUBLIC_USE_INTEGRATION_TEST_ACCESS_KEY ?? '';
+let accessKeyBase64: string = process.env.ACCESS_KEY ?? '';
 if (!accessKeyBase64){
-  throw new Error(`Unable to load accessKey from .env`);
+  throw new Error(`Unable to load ACCESS_KEY from .env`);
 }
 export const OAuthAccessKey: AccessKey = JSON.parse(StringUtils.base64toString(accessKeyBase64) ?? '');
-export const repoId: string = process.env.DEV_CA_PUBLIC_USE_REPOSITORY_ID_1 ?? '';
+export const repoId: string = process.env.REPOSITORY_ID ?? '';
 
