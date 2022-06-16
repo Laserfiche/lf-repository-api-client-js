62be6e675a0b5d210d6c901db382b0e6f0f23aef
Steven Xu <steven.xu@laserfiche.com>
2022-06-16 14:41:33 -0400

    reformatted the config files

--- jest.jsdom.config.js
+++ jest.jsdom.config.js
@@ -1,16 +1,16 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-    preset: 'ts-jest/presets/js-with-ts-esm',
-    globals: {
-      'ts-jest': {
-        useESM: true,
-      },
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
     },
-    testEnvironment: 'jsdom',
-    reporters: ["default", ["jest-junit", { outputName: "junit-jsdom.xml" }]],
-    testTimeout:200000,
-    roots:["test/Attributes/", "test/AccessTokens/","test/FieldDefinitions/"],
-    moduleNameMapper: {
-      '^(\\.{1,2}/.*)\\.js$': '$1',
-    }
-  };
+  },
+  testEnvironment: 'jsdom',
+  reporters: ['default', ['jest-junit', { outputName: 'junit-jsdom.xml' }]],
+  testTimeout: 200000,
+  roots: ['test/Attributes/', 'test/AccessTokens/', 'test/FieldDefinitions/'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
--- jest.node.config.js
+++ jest.node.config.js
@@ -1,16 +1,16 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 export default {
-    preset: 'ts-jest/presets/js-with-ts-esm',
-    globals: {
-      'ts-jest': {
-        useESM: true,
-      },
+  preset: 'ts-jest/presets/js-with-ts-esm',
+  globals: {
+    'ts-jest': {
+      useESM: true,
     },
-    testEnvironment: 'node',
-    reporters: ["default", ["jest-junit", { outputName: "junit-node.xml" }]],
-    testTimeout:200000,
-    roots:["test/Attributes/", "test/AccessTokens/","test/FieldDefinitions/"],
-    moduleNameMapper: {
-      '^(\\.{1,2}/.*)\\.js$': '$1',
-    }
-  };
+  },
+  testEnvironment: 'node',
+  reporters: ['default', ['jest-junit', { outputName: 'junit-node.xml' }]],
+  testTimeout: 200000,
+  roots: ['test/Attributes/', 'test/AccessTokens/', 'test/FieldDefinitions/'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+};
