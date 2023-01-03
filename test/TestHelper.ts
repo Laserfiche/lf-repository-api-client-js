83bb5258e69e0812e6d5b89832a192a8bd7c671c
Steven Xu <steven.xu@laserfiche.com>
2023-01-03 16:20:57 -0500

    refactored testHelper file

--- test/TestHelper.ts
+++ test/TestHelper.ts
@@ -1,21 +1,39 @@
 import 'dotenv/config';
-import { authorizationTypeEnum } from './AuthorizationType.js';
+import { authorizationTypeEnum as authType } from './AuthorizationType.js';
 import { AccessKey, createFromBase64EncodedAccessKey } from '@laserfiche/lf-api-client-core';
-export const testServicePrincipalKey: string = process.env.SERVICE_PRINCIPAL_KEY ?? '';
-if (!testServicePrincipalKey) {
-  throw new Error(`Unable to load SERVICE_PRINCIPAL_KEY from .env`);
+const authorizationType: authType = (process.env.AUTHORIZATION_TYPE ?? authType.None) as authType;
+const testHeader: string = process.env.TEST_HEADER ?? '';
+let testServicePrincipalKey: string;
+let OAuthAccessKey: AccessKey;
+let accessKeyBase64: string;
+let username: string;
+let password: string;
+let baseUrl: string;
+if (!authorizationType) {
+  throw new Error(`Unable to load AUTHORIZATION_TYPE from .env`);
 }
-let accessKeyBase64: string = process.env.ACCESS_KEY ?? '';
-if (!accessKeyBase64) {
-  throw new Error(`Unable to load ACCESS_KEY from .env`);
+const repositoryId: string = process.env.REPOSITORY_ID ?? '';
+if (authorizationType === authType.CloudAccessKey) {
+  testServicePrincipalKey = process.env.SERVICE_PRINCIPAL_KEY ?? '';
+  accessKeyBase64 = process.env.ACCESS_KEY ?? '';
+  OAuthAccessKey = createFromBase64EncodedAccessKey(accessKeyBase64 ?? '');
+} else if (authorizationType === authType.APIServerUsernamePassword) {
+  username = process.env.APISERVER_USERNAME ?? '';
+  password = process.env.APISERVER_PASSWORD ?? '';
+  baseUrl = process.env.APISERVER_REPOSITORY_API_BASE_URL ?? '';
+} else {
+  throw new Error(
+    `"Invalid value for 'AUTHORIZATION_TYPE'. It can only be 'CLOUD_ACCESS_KEY' or 'API_SERVER_USERNAME_PASSWORD'."`
+  );
 }
-export const OAuthAccessKey: AccessKey = createFromBase64EncodedAccessKey(accessKeyBase64 ?? '');
-export const repositoryId: string = process.env.REPOSITORY_ID ?? '';
-export const testHeader: string = process.env.TEST_HEADER ?? '';
-export const authorizationType: authorizationTypeEnum = (process.env.AUTHORIZATION_TYPE ??
-  authorizationTypeEnum.None) as authorizationTypeEnum;
 
-// Self Hosted connection parameters
-export const username: string = process.env.APISERVER_USERNAME ?? '';
-export const password: string = process.env.APISERVER_PASSWORD ?? '';
-export const baseUrl: string = process.env.APISERVER_REPOSITORY_API_BASE_URL ?? '';
+export {
+  OAuthAccessKey,
+  testServicePrincipalKey,
+  testHeader,
+  repositoryId,
+  authorizationType,
+  username,
+  password,
+  baseUrl,
+};
