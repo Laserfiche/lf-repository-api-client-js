474fae1fe4fc7ba646f311c931cac801bee86621
Steven Xu <steven.xu@laserfiche.com>
2023-05-19 17:33:25 -0400

    removed all srs and test files

--- test/AuthorizationType.ts
+++
@@ -1,5 +0,0 @@
-export enum authorizationTypeEnum{
-    CloudAccessKey = "CLOUD_ACCESS_KEY",
-    APIServerUsernamePassword = "API_SERVER_USERNAME_PASSWORD",
-    None = ''
-}
--- test/CreateSession.ts
+++
@@ -1,6 +0,0 @@
-import { repositoryId } from './TestHelper.js';
-import { IRepositoryApiClient } from '../src/index.js';
-import { createClient } from './BaseTest.js';
-let _RepositoryApiClient: IRepositoryApiClient;
-_RepositoryApiClient = createClient();
-export { repositoryId, _RepositoryApiClient };
