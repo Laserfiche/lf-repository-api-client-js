9b5666b51aa48178ba2fb5b2aa5afd0bf7ab9e7e
Jason Jia <jason.jia@laserfiche.com>
2023-08-31 16:30:18 -0400

    Rename and update copy shortcut tests

--- test/Entries/CopyEntryCopyShortcut.test.ts
+++ test/Entries/CopyEntryCopyShortcut.test.ts
@@ -1,41 +1,42 @@
 import { repositoryId } from '../TestHelper.js';
 import {
-  DeleteEntryWithAuditReason,
+  CopyEntryRequest,
+  CreateEntryRequest,
+  CreateEntryRequestEntryType,
   Entry,
   EntryType,
-  PostEntryChildrenEntryType,
-  PostEntryChildrenRequest,
+  StartDeleteEntryRequest,
 } from '../../src/index.js';
 import { _RepositoryApiClient } from '../CreateSession.js';
 import 'isomorphic-fetch';
 
-describe('Create Copy Entry Test', () => {
+describe('Copy Entry Test', () => {
   let createdEntries: Array<Entry> = new Array();
 
   afterEach(async () => {
     for (let i = 0; i < createdEntries.length; i++) {
       if (createdEntries[i]) {
-        let body: DeleteEntryWithAuditReason = new DeleteEntryWithAuditReason();
+        let body: StartDeleteEntryRequest = new StartDeleteEntryRequest();
         let num: number = Number(createdEntries[i].id);
-        await _RepositoryApiClient.entriesClient.deleteEntryInfo({ repoId: repositoryId, entryId: num, request: body });
+        await _RepositoryApiClient.entriesClient.startDeleteEntry({ repositoryId: repositoryId, entryId: num, request: body });
         await new Promise((r) => setTimeout(r, 5000));
       }
     }
     createdEntries = [];
   });
 
-  test('Create Copy Entry Copy Shortcut', async () => {
+  test('Copy Entry Copy Shortcut', async () => {
     //Create new entry
     let newEntryName: string = 'RepositoryApiClientIntegrationTest JS CreateFolder';
     let parentEntryId: number = 1;
-    let request: PostEntryChildrenRequest = new PostEntryChildrenRequest();
-    request.entryType = PostEntryChildrenEntryType.Folder;
+    let request: CreateEntryRequest = new CreateEntryRequest();
+    request.entryType = CreateEntryRequestEntryType.Folder;
     request.name = newEntryName;
-    let response = await _RepositoryApiClient.entriesClient.createOrCopyEntry({
-      repoId: repositoryId,
+    request.autoRename = true;
+    let response = await _RepositoryApiClient.entriesClient.createEntry({
+      repositoryId: repositoryId,
       entryId: parentEntryId,
       request,
-      autoRename: true,
     });
     let targetEntry: Entry = response;
     expect(targetEntry).not.toBeNull();
@@ -45,15 +46,15 @@ describe('Create Copy Entry Test', () => {
 
     //create a shortcut to the new entry
     newEntryName = 'RepositoryApiClientIntegrationTest JS CreateShortcut';
-    request = new PostEntryChildrenRequest();
-    request.entryType = PostEntryChildrenEntryType.Shortcut;
+    request = new CreateEntryRequest();
+    request.entryType = CreateEntryRequestEntryType.Shortcut;
     request.name = newEntryName;
     request.targetId = targetEntry.id;
-    response = await _RepositoryApiClient.entriesClient.createOrCopyEntry({
-      repoId: repositoryId,
+    request.autoRename = true;
+    response = await _RepositoryApiClient.entriesClient.createEntry({
+      repositoryId: repositoryId,
       entryId: parentEntryId,
       request,
-      autoRename: true,
     });
     let shortcut: Entry = response;
     expect(shortcut).not.toBeNull();
@@ -62,14 +63,14 @@ describe('Create Copy Entry Test', () => {
     expect(EntryType.Shortcut).toBe(shortcut.entryType);
 
     //Copy Entry
-    request = new PostEntryChildrenRequest();
-    request.name = 'RepositoryApiClientIntegrationTest JS CopiedEntry';
-    request.sourceId = shortcut.id;
-    let newEntry: Entry = await _RepositoryApiClient.entriesClient.createOrCopyEntry({
-      repoId: repositoryId,
+    let copyRequest = new CopyEntryRequest();
+    copyRequest.name = 'RepositoryApiClientIntegrationTest JS CopiedEntry';
+    copyRequest.sourceId = shortcut.id;
+    copyRequest.autoRename = true;
+    let newEntry: Entry = await _RepositoryApiClient.entriesClient.copyEntry({
+      repositoryId: repositoryId,
       entryId: parentEntryId,
-      request,
-      autoRename: true,
+      request: copyRequest,
     });
     createdEntries.push(newEntry);
     expect(newEntry.parentId).toBe(parentEntryId);
