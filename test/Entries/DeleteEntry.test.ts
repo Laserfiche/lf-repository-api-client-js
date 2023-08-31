8af87b79711b0192c3c8b8970ed23bd8abaa486e
Jason Jia <jason.jia@laserfiche.com>
2023-08-31 16:31:59 -0400

    Update delete entry test

--- test/Entries/DeleteEntry.test.ts
+++ test/Entries/DeleteEntry.test.ts
@@ -1,20 +1,20 @@
 import { repositoryId } from '../TestHelper.js';
 import { CreateEntry } from '../BaseTest.js';
-import { DeleteEntryWithAuditReason } from '../../src/index.js';
 import { _RepositoryApiClient } from '../CreateSession.js';
 import 'isomorphic-fetch';
+import { StartDeleteEntryRequest } from '../../src/index.js';
 
 describe('Delete Entries Integration Tests', () => {
   test('Delete Entry', async () => {
     let deleteEntry = await CreateEntry(_RepositoryApiClient, 'RepositoryApiClientIntegrationTest JS DeleteFolder');
-    let body: DeleteEntryWithAuditReason = new DeleteEntryWithAuditReason();
-    let result = await _RepositoryApiClient.entriesClient.deleteEntryInfo({
-      repoId: repositoryId,
+    let body: StartDeleteEntryRequest = new StartDeleteEntryRequest();
+    let result = await _RepositoryApiClient.entriesClient.startDeleteEntry({
+      repositoryId: repositoryId,
       entryId: deleteEntry.id ?? -1,
       request: body,
     });
-    let token: string = result.token ?? '';
-    expect(token).not.toBeNull();
-    expect(token).not.toBe('');
+    let taskId: string = result.taskId ?? '';
+    expect(taskId).not.toBeNull();
+    expect(taskId).not.toBe('');
   });
 });
