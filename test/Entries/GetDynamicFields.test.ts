a0c6bc35e63710e16a7774fa6dfda7db896368f4
Jason Jia <jason.jia@laserfiche.com>
2023-08-31 16:36:20 -0400

    Update get dynamic fields test

--- test/Entries/GetDynamicFields.test.ts
+++ test/Entries/GetDynamicFields.test.ts
@@ -1,5 +1,5 @@
 import { repositoryId } from '../TestHelper.js';
-import { GetDynamicFieldLogicValueRequest, TemplateDefinitionCollectionResponse } from '../../src/index.js';
+import { ListDynamicFieldValuesRequest, TemplateDefinitionCollectionResponse } from '../../src/index.js';
 import { _RepositoryApiClient } from '../CreateSession.js';
 import 'isomorphic-fetch';
 
@@ -7,17 +7,17 @@ describe('Dynamic Fields Integration Tests', () => {
   let entryId: number = 1;
   test('Get Dynamic Fields Entry', async () => {
     let templateDefinitionResponse: TemplateDefinitionCollectionResponse =
-      await _RepositoryApiClient.templateDefinitionsClient.getTemplateDefinitions({ repoId: repositoryId });
+      await _RepositoryApiClient.templateDefinitionsClient.listTemplateDefinitions({ repositoryId: repositoryId });
     let templateDefinitions = templateDefinitionResponse.value;
     if (!templateDefinitions) {
       throw new Error('templateDefinitions is undefined');
     }
     expect(templateDefinitions).not.toBeNull();
     expect(templateDefinitions?.length).toBeGreaterThan(0);
-    let request = new GetDynamicFieldLogicValueRequest();
-    request.templateId = templateDefinitions[0].id;
-    let dynamicFieldValueResponse = await _RepositoryApiClient.entriesClient.getDynamicFieldValues({
-      repoId: repositoryId,
+    let request = new ListDynamicFieldValuesRequest();
+    request.templateId = templateDefinitions[0].id!;
+    let dynamicFieldValueResponse = await _RepositoryApiClient.entriesClient.listDynamicFieldValues({
+      repositoryId,
       entryId,
       request,
     });
