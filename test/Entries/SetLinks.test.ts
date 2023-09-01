02d00af05bf6020ebafedc1df6f9cbb01b8cf741
Jason Jia <jason.jia@laserfiche.com>
2023-09-01 14:08:19 -0400

    Update set links test

--- test/Entries/SetLinks.test.ts
+++ test/Entries/SetLinks.test.ts
@@ -1,6 +1,5 @@
 import { repositoryId, authorizationType } from '../TestHelper.js';
-import { authorizationTypeEnum } from '../AuthorizationType.js';
-import { DeleteEntryWithAuditReason, Entry, PutLinksRequest, WEntryLinkInfo } from '../../src/index.js';
+import { Entry, LinkToUpdate, SetLinksRequest, StartDeleteEntryRequest } from '../../src/index.js';
 import { CreateEntry } from '../BaseTest.js';
 import { _RepositoryApiClient } from '../CreateSession.js';
 import 'isomorphic-fetch';
@@ -11,15 +10,12 @@ describe('Set Entries Integration Tests', () => {
   afterEach(async () => {
     for (let i = 0; i < createdEntries.length; i++) {
       if (createdEntries[i]) {
-        let body: DeleteEntryWithAuditReason = new DeleteEntryWithAuditReason();
-        let num: number = Number(createdEntries[i].id);
-        await _RepositoryApiClient.entriesClient.deleteEntryInfo({ repoId: repositoryId, entryId: num, request: body });
+        let request = new StartDeleteEntryRequest();
+        let entryId = createdEntries[i].id!;
+        await _RepositoryApiClient.entriesClient.startDeleteEntry({ repositoryId, entryId, request });
       }
     }
     createdEntries = [];
-    if (authorizationType === authorizationTypeEnum.CloudAccessKey) {
-      _RepositoryApiClient.serverSessionClient.invalidateServerSession({ repoId: repositoryId });
-    }
   });
 
   test('Set Links', async () => {
@@ -27,25 +23,28 @@ describe('Set Entries Integration Tests', () => {
       _RepositoryApiClient,
       'RepositoryApiClientIntegrationTest JS SetLinks Source'
     );
+    
     createdEntries.push(sourceEntry);
     var targetEntry = await CreateEntry(_RepositoryApiClient, 'RepositoryApiClientIntegrationTest JS SetLinks Target');
+    
     createdEntries.push(targetEntry);
-    let putLinks = new PutLinksRequest();
-    putLinks.targetId = targetEntry.id;
-    putLinks.linkTypeId = 1;
-    let request = new Array<PutLinksRequest>(putLinks);
-    let result = await _RepositoryApiClient.entriesClient.assignEntryLinks({
-      repoId: repositoryId,
+    
+    let request = new SetLinksRequest();
+    let linkToUpdate = new LinkToUpdate();
+    linkToUpdate.linkDefinitionId = 1;
+    linkToUpdate.otherEntryId = targetEntry.id;
+    request.links = [linkToUpdate];
+    
+    let result = await _RepositoryApiClient.entriesClient.setLinks({
+      repositoryId,
       entryId: sourceEntry.id ?? -1,
-      linksToAdd: request,
+      request,
     });
 
-    let links: WEntryLinkInfo[] | undefined = result.value;
-    if (!links) {
-      throw new Error('links is undefined');
-    }
+    let links = result.value!;
+    
     expect(links).not.toBeNull();
-    expect(request.length).toBe(links.length);
+    expect(request.links!.length).toBe(links.length);
     expect(sourceEntry.id).toBe(links[0].sourceId);
     expect(targetEntry.id).toBe(links[0].targetId);
   });
