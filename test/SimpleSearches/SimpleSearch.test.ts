cde4b12373f0a13c007f0c31a2eac1503184994d
Jason Jia <jason.jia@laserfiche.com>
2023-09-01 16:12:05 -0400

    Rename and update simple search test

--- test/SimpleSearches/SimpleSearch.test.ts
+++ test/SimpleSearches/SimpleSearch.test.ts
@@ -1,15 +1,15 @@
 import { repositoryId } from '../TestHelper.js';
-import { SimpleSearchRequest } from '../../src/index.js';
 import { _RepositoryApiClient } from '../CreateSession.js';
 import 'isomorphic-fetch';
+import { SearchEntryRequest } from '../../src/index.js';
 
 describe('Simple Search Integration Tests', () => {
 
   test('Create Simple Search', async () => {
-    let request: SimpleSearchRequest = new SimpleSearchRequest();
+    let request = new SearchEntryRequest();
     request.searchCommand = "({LF:Basic ~= \"search text\", option=\"DFANLT\"})";
-    let simpleSearchResponse = await _RepositoryApiClient.simpleSearchesClient.createSimpleSearchOperation({
-      repoId: repositoryId,
+    let simpleSearchResponse = await _RepositoryApiClient.simpleSearchesClient.searchEntry({
+      repositoryId,
       request,
     });
     expect(simpleSearchResponse.value).not.toBeNull();
