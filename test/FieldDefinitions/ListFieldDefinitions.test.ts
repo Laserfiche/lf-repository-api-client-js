2b23ecbb4b16870d3101d1858ad77e5c8d126047
Jason Jia <jason.jia@laserfiche.com>
2023-09-01 14:17:28 -0400

    Update and rename list definitions tests

--- test/FieldDefinitions/ListFieldDefinitions.test.ts
+++ test/FieldDefinitions/ListFieldDefinitions.test.ts
@@ -1,35 +1,37 @@
 import { repositoryId } from '../TestHelper.js';
-import { FieldDefinitionCollectionResponse, WFieldInfo } from '../../src/index.js';
+import { FieldDefinitionCollectionResponse } from '../../src/index.js';
 import { _RepositoryApiClient } from '../CreateSession.js';
 import 'isomorphic-fetch';
 
 describe('Field Definitions Integration Tests', () => {
   test('Get Field Definitions', async () => {
     let result: FieldDefinitionCollectionResponse =
-      await _RepositoryApiClient.fieldDefinitionsClient.getFieldDefinitions({ repoId: repositoryId });
-    expect(result.value).not.toBeNull();
+      await _RepositoryApiClient.fieldDefinitionsClient.listFieldDefinitions({ repositoryId });
+    
+      expect(result.value).not.toBeNull();
   });
 
   test('Get Field Definitions simple paging', async () => {
     let maxPageSize = 1;
     let prefer = `maxpagesize=${maxPageSize}`;
-    let response = await _RepositoryApiClient.fieldDefinitionsClient.getFieldDefinitions({ repoId: repositoryId, prefer });
-    if (!response.value) {
-      throw new Error('response.value is undefined');
-    }
+    let response = await _RepositoryApiClient.fieldDefinitionsClient.listFieldDefinitions({ repositoryId, prefer });
+    
     expect(response).not.toBeNull();
+    expect(response.value).not.toBeNull();
+    
     let nextLink: string = response.odataNextLink ?? '';
+    
     expect(nextLink).not.toBeNull();
-    expect(response.value.length).toBeLessThanOrEqual(maxPageSize);
+    expect(response.value!.length).toBeLessThanOrEqual(maxPageSize);
+    
     let response2 = await _RepositoryApiClient.fieldDefinitionsClient.listFieldDefinitionsNextLink({
       nextLink,
       maxPageSize,
     });
-    if (!response2.value) {
-      throw new Error('response2.value is undefined');
-    }
+
     expect(response2).not.toBeNull();
-    expect(response2.value.length).toBeLessThanOrEqual(maxPageSize);
+    expect(response2.value).not.toBeNull();
+    expect(response2.value!.length).toBeLessThanOrEqual(maxPageSize);
   });
 
   test('Get Field Definitions for each paging', async () => {
@@ -45,26 +47,26 @@ describe('Field Definitions Integration Tests', () => {
       return true;
     };
     await _RepositoryApiClient.fieldDefinitionsClient.listFieldDefinitionsForEach({ callback, repoId: repositoryId, maxPageSize });
+    
     expect(entries).toBeGreaterThan(0);
     expect(pages).toBeGreaterThan(0);
   });
 
   test('Get Field Definitions by Id', async () => {
     let FieldDefResponse: FieldDefinitionCollectionResponse =
-      await _RepositoryApiClient.fieldDefinitionsClient.getFieldDefinitions({ repoId: repositoryId });
+      await _RepositoryApiClient.fieldDefinitionsClient.listFieldDefinitions({ repositoryId });
     if (!FieldDefResponse.value) {
       throw new Error('FieldDefResponse.value is undefined');
     }
-    let firstFieldDef: WFieldInfo = FieldDefResponse.value[0];
-    if (!firstFieldDef) {
-      throw new Error('firstFieldDef is undefined');
-    }
-    expect(firstFieldDef).not.toBeNull();
-    let response = await _RepositoryApiClient.fieldDefinitionsClient.getFieldDefinitionById({
-      repoId: repositoryId,
-      fieldDefinitionId: firstFieldDef.id ?? -1,
+    let fieldDefinition1 = FieldDefResponse.value[0];
+    
+    expect(fieldDefinition1).not.toBeNull();
+    
+    let response = await _RepositoryApiClient.fieldDefinitionsClient.getFieldDefinition({
+      repositoryId,
+      fieldId: fieldDefinition1.id ?? -1,
     });
-    let fieldDef = response;
-    expect(fieldDef.id).toBe(firstFieldDef.id);
+
+    expect(response.id).toBe(fieldDefinition1.id);
   });
 });
