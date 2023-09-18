7e2b4da100abe98cf8f1eacb90d439bd4a40892a
Samad Paydar <samad.paydar@laserfiche.com>
2023-09-18 11:52:17 -0400

    Update integration test.

--- test/Entries/LargeFileImport.test.ts
+++ test/Entries/LargeFileImport.test.ts
@@ -2,12 +2,12 @@ import { repositoryId } from '../TestHelper.js';
 import { _RepositoryApiClient } from '../CreateSession.js';
 import 'isomorphic-fetch';
 import { Blob as NodeBlob } from 'buffer';
-import { FileParameter, ImportEntryRequest, StartTaskResponse, TaskStatus } from '../../src/index.js';
+import { FileParameter, ImportAsyncMetadata, ImportEntryRequest, StartTaskResponse, TaskStatus } from '../../src/index.js';
 import { isBrowser } from '@laserfiche/lf-js-utils/dist/utils/core-utils.js';
 
 describe('Large File Import Integration Tests', () => {
 
-    test('Importing a 60MB file is successful', async () => {
+      test('Importing a 60MB file is successful', async () => {
         let blob: any;
         if (isBrowser()){
             blob = new Blob([""], {
@@ -22,19 +22,29 @@ describe('Large File Import Integration Tests', () => {
             fileName: "test/Entries/sampleFiles/60MB.pdf",
             data: blob
           }
+
+          var name = "sample.pdf";
+          var templateName = "Email";
+          var tagName = "TestTag";
+          var mimeType = "application/pdf";
+          var rootEntryId = 1;
           const request = new ImportEntryRequest();
           request.autoRename = true;
-          request.name = "sample.pdf";
-          let response: StartTaskResponse = await _RepositoryApiClient.entriesClient.startImport({
-            fileName: "sample.pdf",
-            fileSizeInBytes: 63096545,
-            mimeType: "application/pdf",
+          request.name = name;
+          var metadata ={
+            templateName: templateName,
+            tags: [tagName]
+          };
+      
+          request.metadata = ImportAsyncMetadata.fromJS(metadata);
+          let response: StartTaskResponse = await _RepositoryApiClient.entriesClient.startImportEntry({
             repositoryId,
-            entryId: 1,
+            entryId: rootEntryId,
             file: edoc,
+            fileSizeInBytes: 63096545,
+            mimeType: mimeType,
             request: request
         });
-        await new Promise((r) => setTimeout(r, 30000));
 
         var response2 = await _RepositoryApiClient.tasksClient.listTasks({
             repositoryId,
@@ -46,10 +56,23 @@ describe('Large File Import Integration Tests', () => {
         expect(response2.value!.length).toBeGreaterThan(0);
     
         let taskProgress = response2.value![0];
-        console.log(response2);
-        console.log(taskProgress.errors);
         expect(taskProgress.status).toBe(TaskStatus.Completed);
         expect(taskProgress.percentComplete).toBe(100);
-      });
+        let createdEntryId = taskProgress.result?.entryId;
+        var createdEntry = await _RepositoryApiClient.entriesClient.getEntry({repositoryId: repositoryId, entryId: createdEntryId!});
+        expect(createdEntry).not.toBeNull();
+        expect(createdEntry.id).toEqual(createdEntryId);
+        expect(createdEntry.name).toEqual(name);
+        expect(createdEntry.isContainer).toBeFalsy();
+        expect(createdEntry.isLeaf).toBeTruthy();
+        expect(createdEntry.parentId).toEqual(rootEntryId);
+        
+        // Verify the created entry's templateName
+        expect(createdEntry.templateName).toEqual(templateName);
 
+        // Verify the created entry's tag
+        var tags = await _RepositoryApiClient.entriesClient.listTags({repositoryId: repositoryId, entryId: createdEntryId!});
+        expect(tags.value?.length).toEqual(1);
+        expect(tags.value![0].name).toEqual(tagName);
+      });
   });
