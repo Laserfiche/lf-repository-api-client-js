b475e7c49e21f34532c633105f5b7a6ffcdc7a2c
Samad Paydar <samad.paydar@laserfiche.com>
2023-10-24 16:46:24 -0400

    Add copyright header to *.ts files.

--- test/Entries/RemoveEntries.test.ts
+++ test/Entries/RemoveEntries.test.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Laserfiche.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
 import { repositoryId } from '../TestHelper.js';
 import { Entry, SetTemplateRequest, StartDeleteEntryRequest } from '../../src/index.js';
 import { allFalse, CreateEntry } from '../BaseTest.js';
